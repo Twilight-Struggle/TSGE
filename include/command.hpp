2ad2fe09b21871f6e2be269f7818f90dae2423a9
Twilight-Struggle <pikupiken@yahoo.co.jp>
2025-06-18 23:06:57 +0900

    Commandのcard&を各クラスに配分

--- include/command.hpp
+++ include/command.hpp
@@ -13,8 +13,7 @@ class Move;
 
 class Command {
  public:
-  Command(Side side, const std::unique_ptr<Card>& card)
-      : side_{side}, card_{card} {};
+  Command(Side side) : side_{side} {};
   virtual ~Command() = default;
   virtual bool apply(Board& board) const = 0;
   // MCTSで必要
@@ -22,7 +21,6 @@ class Command {
 
  protected:
   const Side side_;
-  const std::unique_ptr<Card>& card_;
 };
 
 using CommandPtr = std::shared_ptr<Command>;
@@ -31,11 +29,12 @@ class ActionPlaceInfluence : public Command {
  public:
   ActionPlaceInfluence(Side side, const std::unique_ptr<Card>& card,
                        const std::map<CountryEnum, int>& targetCountries)
-      : Command{side, card}, targetCountries_{targetCountries} {};
+      : Command{side}, card_{card}, targetCountries_{targetCountries} {};
 
   bool apply(Board& board) const override;
 
  private:
+  const std::unique_ptr<Card>& card_;
   const std::map<CountryEnum, int> targetCountries_;
 };
 
@@ -43,11 +42,12 @@ class ActionRealigment : public Command {
  public:
   ActionRealigment(Side side, const std::unique_ptr<Card>& card,
                    CountryEnum targetCountry)
-      : Command{side, card}, targetCountry_{targetCountry} {};
+      : Command{side}, card_{card}, targetCountry_{targetCountry} {};
 
   bool apply(Board& board) const override;
 
  private:
+  const std::unique_ptr<Card>& card_;
   const CountryEnum targetCountry_;
 };
 
@@ -55,52 +55,45 @@ class ActionCoup : public Command {
  public:
   ActionCoup(Side side, const std::unique_ptr<Card>& card,
              CountryEnum targetCountry)
-      : Command{side, card}, targetCountry_{targetCountry} {};
+      : Command{side}, card_{card}, targetCountry_{targetCountry} {};
 
   bool apply(Board& board) const override;
 
  private:
+  const std::unique_ptr<Card>& card_;
   const CountryEnum targetCountry_;
 };
 
 class ActionSpaceRace : public Command {
  public:
   ActionSpaceRace(Side side, const std::unique_ptr<Card>& card)
-      : Command{side, card} {};
+      : Command{side}, card_{card} {};
 
   bool apply(Board& board) const override;
+
+ private:
+  const std::unique_ptr<Card>& card_;
 };
 
 class ChangeDefconCommand : public Command {
  public:
   explicit ChangeDefconCommand(int delta)
-      : Command(Side::USSR, getNullCard()), delta_{delta} {}
+      : Command(Side::USSR), delta_{delta} {}
 
   bool apply(Board& board) const override;
 
  private:
   const int delta_;
-
-  static const std::unique_ptr<Card>& getNullCard() {
-    static std::unique_ptr<Card> nullCard;
-    return nullCard;
-  }
 };
 
 class ChangeVPCommand : public Command {
  public:
-  explicit ChangeVPCommand(int delta)
-      : Command(Side::USSR, getNullCard()), delta_{delta} {}
+  explicit ChangeVPCommand(int delta) : Command(Side::USSR), delta_{delta} {}
 
   bool apply(Board& board) const override;
 
  private:
   const int delta_;
-
-  static const std::unique_ptr<Card>& getNullCard() {
-    static std::unique_ptr<Card> nullCard;
-    return nullCard;
-  }
 };
 
 class Request : public Command {
@@ -108,17 +101,11 @@ class Request : public Command {
   Request(Side side,
           std::function<std::vector<std::unique_ptr<Move>>(const Board&)>
               legalMoves)
-      : Command(side, getNullCard()), legalMoves(std::move(legalMoves)) {}
+      : Command(side), legalMoves(std::move(legalMoves)) {}
 
   std::function<std::vector<std::unique_ptr<Move>>(const Board&)> legalMoves;
   // std::function<std::vector<CommandPtr>(const Move&)> resume; いらないかも
 
   bool apply(Board&) const override { return true; }
   Side getSide() const { return side_; }
-
- private:
-  static const std::unique_ptr<Card>& getNullCard() {
-    static std::unique_ptr<Card> nullCard;
-    return nullCard;
-  }
 };
